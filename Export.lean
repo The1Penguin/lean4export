816e75f6065fe0551c6ba9648c5417f2c9697e48
pingu <nor@acorneroftheweb.com>
2025-02-25 16:13:41 +0100

    Now json exists?

--- Export.lean
+++ Export.lean
@@ -2,6 +2,94 @@ import Lean
 
 open Lean
 
+instance : ToJson BinderInfo where
+  toJson
+  | .default        => .str "default"
+  | .implicit       => .str "implicit"
+  | .strictImplicit => .str "strictImplicit"
+  | .instImplicit   => .str "instImplicit"
+
+instance : ToJson Literal where
+  toJson
+  | .natVal val => .num val
+  | .strVal val => .str val
+
+instance : ToJson QuotKind where
+  toJson
+  | .type => .str "type"
+  | .ctor => .str "ctor"
+  | .lift => .str "lift"
+  | .ind  => .str "ind"
+
+instance : ToJson MData where
+  toJson
+  | _k => .null -- TODO: Complains with the following message
+  -- elaboration function for 'term.pseudo.antiquot' has not been implemented $k.entries
+
+partial def Level.toJson : Level -> Json
+  | .zero         => .num 0
+  | .succ l       =>
+    have : ToJson Level := ⟨Level.toJson⟩
+    json% { "succ" : $l }
+  | .max n m      =>
+    have : ToJson Level := ⟨Level.toJson⟩
+    json% { "max" : [$n,$m] }
+  | .imax n m     =>
+    have : ToJson Level := ⟨Level.toJson⟩
+    json% { "imax" : [$n,$m] }
+  | .param name   => json% { "param" : $name }
+  | .mvar LMVarId => json% { mvar : $LMVarId.name }
+instance : ToJson Level := ⟨Level.toJson⟩
+
+partial def Expr.toJson : Expr -> Json
+  | .bvar (deBruijnIndex : Nat) => json% { "bVar" : { "index" : $deBruijnIndex } }
+  | .fvar (fvarId : FVarId) => json% { "fVar" : { "id" : $fvarId }}
+  | .mvar (mvarId : MVarId) => json% { "mVar" : { "id" : $mvarId }}
+  | .sort (u : Level) => json% { "sort" : { "level" : $u }}
+  | .const (declName : Name) (us : List Level) => json% { "const" : {"declName" : $declName, "levels" : $us }}
+  | .app (fn : Expr) (arg : Expr) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "app" : { "fn" : $fn, "arg" : $arg }}
+  | .lam (binderName : Name) (binderType : Expr) (body : Expr) (binderInfo : BinderInfo) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "lambda" : { "binderName" : $binderName, "binderType" : $binderType, "body" : $body, "binderInfo" : $binderInfo} }
+  | .forallE (binderName : Name) (binderType : Expr) (body : Expr) (binderInfo : BinderInfo) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "forAllE" : { "binderName" : $binderName, "binderType" : $binderType, "body" : $body, "binderInfo" : $binderInfo} }
+  | .letE (declName : Name) (type : Expr) (value : Expr) (body : Expr) (nonDep : Bool) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "letE" : { "declName" : $declName, "type" : $type, "value" : $value, "body" : $body, "nonDep" : $nonDep} }
+  | .lit (l : Literal)  => json% { "literal" : $l }
+  | .mdata (data : MData) (expr : Expr) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "mData" : { "data" : $data, "expr" : $expr }}
+  | .proj (typeName : Name) (idx : Nat) (struct : Expr) =>
+      have : ToJson Expr := ⟨Expr.toJson⟩
+      json% { "proj" : { "typeName" : $typeName, "idx" : $idx, "struct" : $struct } }
+
+instance : ToJson Expr := ⟨Expr.toJson⟩
+
+
+instance : ToJson AxiomVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type}⟩
+instance : ToJson DefinitionVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "val" : $s.value } ⟩
+instance : ToJson TheoremVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "val" : $s.value}⟩
+instance : ToJson OpaqueVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "val" : $s.value}⟩
+instance : ToJson QuotVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "kind" : $s.kind}⟩
+instance : ToJson InductiveVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "numParams" : $s.numParams, "ctors" : $s.ctors}⟩
+instance : ToJson ConstructorVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type, "induct" : $s.induct}⟩
+instance : ToJson RecursorVal := ⟨λ s => json% { "name" : $s.name, "type" : $s.type}⟩
+
+instance : ToJson (Name × ConstantInfo) where
+  toJson
+    | (n, .axiomInfo  v) => json% { "name" : $n, "axiomVal" : $v}
+    | (n, .defnInfo   v) => json% { "name" : $n, "definitionalVal" : $v}
+    | (n, .thmInfo    v) => json% { "name" : $n, "theoremVal" : $v}
+    | (n, .opaqueInfo v) => json% { "name" : $n, "opaqueVal" : $v}
+    | (n, .quotInfo   v) => json% { "name" : $n, "quotVal" : $v}
+    | (n, .inductInfo v) => json% { "name" : $n, "inductiveVal" : $v}
+    | (n, .ctorInfo   v) => json% { "name" : $n, "constructorVal" : $v}
+    | (n, .recInfo    v) => json% { "name" : $n, "recursorVal" : $v}
+
 instance : Hashable RecursorRule where
   hash r := hash (r.ctor, r.nfields, r.rhs)
 
@@ -22,177 +110,44 @@ def M.run (env : Environment) (act : M α) : IO α :=
     ReaderT.run (r := { env }) do
       act
 
-@[inline]
-def getIdx [Hashable α] [BEq α] (x : α) (getM : State → HashMap α Nat) (setM : State → HashMap α Nat → State) (rec : M String) : M Nat := do
-  let m ← getM <$> get
-  if let some idx := m.find? x then
-    return idx
-  let s ← rec
-  let m ← getM <$> get
-  let idx := m.size
-  IO.println s!"{idx} {s}"
-  modify fun st => setM st ((getM st).insert x idx)
-  return idx
-
-def dumpName (n : Name) : M Nat := getIdx n (·.visitedNames) ({ · with visitedNames := · }) do
-  match n with
-  | .anonymous => unreachable!
-  | .str n s => return s!"#NS {← dumpName n} {s}"
-  | .num n i => return s!"#NI {← dumpName n} {i}"
-
-def dumpLevel (l : Level) : M Nat := getIdx l (·.visitedLevels) ({ · with visitedLevels := · }) do
-  match l with
-  | .zero | .mvar _ => unreachable!
-  | .succ l => return s!"#US {← dumpLevel l}"
-  | .max l1 l2 => return s!"#UM {← dumpLevel l1} {← dumpLevel l2}"
-  | .imax l1 l2 => return s!"#UIM {← dumpLevel l1} {← dumpLevel l2}"
-  | .param n => return s!"#UP {← dumpName n}"
-
-def seq [ToString α] (xs : List α) : String :=
-  xs.map toString |> String.intercalate " "
-
-def dumpInfo : BinderInfo → String
-  | .default => "#BD"
-  | .implicit => "#BI"
-  | .strictImplicit => "#BS"
-  | .instImplicit => "#BC"
-
-def uint8ToHex (c : UInt8) : String :=
-  let d2 := c / 16
-  let d1 := c % 16
-  (hexDigitRepr d2.toNat ++ hexDigitRepr d1.toNat).toUpper
-
-/-
-Eliminate `mdata` nodes, while dumping an expression.
-
-When an `mdata` node is encountered, skip it. For everything else, update `e`
-IFF a sub-expression contained `mdata`.
-
-returns `(had mdata, the expression with any mdata removed, e's export index)`
--/
-@[inline]
-partial def dumpExprAux (e : Expr) : M (Bool × Expr × Nat) := do
-  /- If this is an mdata node, dump the inner item and return the expr without mdata -/
-  if let .mdata _ e' := e then
-    let (_, e'', idx) ← dumpExprAux e'
-    return (true, e'', idx)
-  /- If we've already seen this expression, use the cached data -/
-  if let some idx := (← get).visitedExprs.find? e then
-    return (false, e, idx)
-  else
-    let (ck, e, s) ←
-      match e with
-      | .mdata .. | .fvar .. | .mvar .. => unreachable!
-      | .bvar i => pure (false, e, (return s!"#EV {i}"))
-      | .sort l => pure (false, e, (return s!"#ES {← dumpLevel l}"))
-      | .const n us => pure (false, e, (return s!"#EC {← dumpName n} {← seq <$> us.mapM dumpLevel}"))
-      | .lit (.natVal i) => pure (false, e, (return s!"#ELN {i}"))
-      | .lit (.strVal s) => pure (false, e, (return s!"#ELS {s.toUTF8.toList.map uint8ToHex |> seq}"))
-      | .app f a =>
-        let (f_rebuilt, f, f_idx) ← dumpExprAux f
-        let (a_rebuilt, a, a_idx) ← dumpExprAux a
-        let ck := f_rebuilt || a_rebuilt
-        pure (ck, if ck then e.updateApp! f a else e, (return s!"#EA {f_idx} {a_idx}"))
-      | .lam n d b bi =>
-        let (d_rebuilt, d, d_idx) ← dumpExprAux d
-        let (b_rebuilt, b, b_idx) ← dumpExprAux b
-        let ck := (d_rebuilt || b_rebuilt)
-        pure (ck, if ck then e.updateLambdaE! d b else e, (return s!"#EL {dumpInfo bi} {← dumpName n} {d_idx} {b_idx}"))
-      | .letE n d v b _ =>
-        let (d_rebuilt, d, d_idx) ← dumpExprAux d
-        let (v_rebuilt, v, v_idx) ← dumpExprAux v
-        let (b_rebuilt, b, b_idx) ← dumpExprAux b
-        let ck := (d_rebuilt || v_rebuilt || b_rebuilt)
-        pure (ck, if ck then e.updateLet! d v b else e, (return s!"#EZ {← dumpName n} {d_idx} {v_idx} {b_idx}"))
-      | .forallE n d b bi =>
-        let (d_rebuilt, d, d_idx) ← dumpExprAux d
-        let (b_rebuilt, b, b_idx) ← dumpExprAux b
-        let ck := (d_rebuilt || b_rebuilt)
-        pure (ck, if ck then e.updateForallE! d b else e, (return s!"#EP {dumpInfo bi} {← dumpName n} {d_idx} {b_idx}"))
-      | .proj s i e2 =>
-        let (e_rebuilt, ex, e'_idx) ← dumpExprAux e2
-        let ck := e_rebuilt
-        pure (ck, if ck then (e.updateProj! ex) else e, (return s!"#EJ {← dumpName s} {i} {e'_idx}"))
-    tryPrintE ck e s
-where
-  tryPrintE (hadMData: Bool) (e : Expr) (s : M String) : M (Bool × Expr × Nat) := do
-    /- If the expression had `mdata` and was rebuilt, check the rebuilt expression
-    against the cache once more -/
-    if hadMData then
-      if let some idx := (← get).visitedExprs.find? e then
-      return (hadMData, e, idx)
-    let idx := (← get).visitedExprs.size
-    modify (fun st => { st with visitedExprs := st.visitedExprs.insert e idx })
-    IO.println s!"{idx} {← s}"
-    return (hadMData, e, idx)
-
-@[inline]
-def dumpExpr (e : Expr) : M Nat := do
-  let (_, _, n) ← dumpExprAux e
-  return n
-
-def dumpHints : ReducibilityHints → String
-  | .opaque => "O"
-  | .abbrev => "A"
-  | .regular n => s!"R {n}"
-
-partial def dumpConstant (c : Name) : M Unit := do
+partial def dumpConstant (c : Name) : M (List Json) := do
   if (← get).visitedConstants.contains c then
-    return
+    return .nil
   modify fun st => { st with visitedConstants := st.visitedConstants.insert c }
   match (← read).env.find? c |>.get! with
   | .axiomInfo val => do
     if val.isUnsafe then
-      return
-    dumpDeps val.type
-    IO.println s!"#AX {← dumpName c} {← dumpExpr val.type} {← seq <$> val.levelParams.mapM dumpName}"
+      return .nil
+    return [(toJson (c, Lean.ConstantInfo.axiomInfo val) )]
   | .defnInfo val => do
     if val.safety != .safe then
-      return
-    dumpDeps val.type
-    dumpDeps val.value
-    IO.println s!"#DEF {← dumpName c} {← dumpExpr val.type} {← dumpExpr val.value} {dumpHints val.hints} {← seq <$> val.levelParams.mapM dumpName}"
+      return .nil
+    return [(toJson (c, Lean.ConstantInfo.defnInfo val) )]
   | .thmInfo val => do
-    dumpDeps val.type
-    dumpDeps val.value
-    IO.println s!"#THM {← dumpName c} {← dumpExpr val.type} {← dumpExpr val.value} {← seq <$> val.levelParams.mapM dumpName}"
+    return [(toJson (c, Lean.ConstantInfo.thmInfo val) )]
   | .opaqueInfo val => do
     if val.isUnsafe then
-      return
-    dumpDeps val.type
-    dumpDeps val.value
-    IO.println s!"#OPAQ {← dumpName c} {← dumpExpr val.type} {← dumpExpr val.value} {← seq <$> val.levelParams.mapM dumpName}"
+      return .nil
+    return [(toJson (c, Lean.ConstantInfo.opaqueInfo val) )]
   | .quotInfo val =>
-    dumpDeps val.type
-    IO.println s!"#QUOT {← dumpName c} {← dumpExpr val.type} {← seq <$> val.levelParams.mapM dumpName}"
+    return [(toJson (c, Lean.ConstantInfo.quotInfo val) )]
   | .inductInfo val => do
     if val.isUnsafe then
-      return
-    dumpDeps val.type
+      return .nil
+    let mut n := []
     for ctor in val.ctors do
-      dumpDeps ((← read).env.find? ctor |>.get!.type)
-    let indNameIdxs ← val.all.mapM dumpName
-    let ctorNameIdxs ← val.ctors.mapM (fun ctor => dumpName ctor)
-    let isRec := if val.isRec then 1 else 0
-    let isNested := if val.isNested then 1 else 0
-    IO.println s!"#IND {← dumpName c} {← dumpExpr val.type} {isRec} {isNested} {val.numParams} {val.numIndices} {indNameIdxs.length} {seq indNameIdxs} {val.numCtors} {seq ctorNameIdxs} {← seq <$> val.levelParams.mapM dumpName}"
+      let d <- dumpConstant ( (← read).env.find? ctor |>.get!.name)
+      n := List.append d n
+    let temp := toJson (c, Lean.ConstantInfo.inductInfo val)
+    let new := match temp.getObjVal? "inductiveVal" with
+                  | .ok j => j.setObjVal! "ctors" (toJson n)
+                  | .error _ => sorry
+    return [temp.setObjVal! "inductiveVal" new ]
   | .ctorInfo val =>
     if val.isUnsafe then
-      return
-    dumpDeps val.type
-    IO.println s!"#CTOR {← dumpName c} {← dumpExpr val.type} {← dumpName val.induct} {val.cidx} {val.numParams} {val.numFields} {← seq <$> val.levelParams.mapM dumpName}"
+      return .nil
+    return [(toJson (c, Lean.ConstantInfo.ctorInfo val) )]
   | .recInfo val =>
     if val.isUnsafe then
-      return
-    dumpDeps val.type
-    let indNameIdxs ← val.all.mapM dumpName
-    let k := if val.k then 1 else 0
-    let ruleIdxs ← val.rules.mapM (fun rule => dumpRecRule rule)
-    IO.println s!"#REC {← dumpName c} {← dumpExpr val.type} {indNameIdxs.length} {seq indNameIdxs} {val.numParams} {val.numIndices} {val.numMotives} {val.numMinors} {ruleIdxs.length} {seq ruleIdxs} {k} {← seq <$> val.levelParams.mapM dumpName}"
-where
-  dumpDeps e := do
-    for c in e.getUsedConstants do
-      dumpConstant c
-  dumpRecRule (rule : RecursorRule) : M Nat := getIdx rule (·.visitedRecRules) ({ · with visitedRecRules := · }) do
-    dumpDeps (rule.rhs)
-    return s!"#RR {← dumpName rule.ctor} {rule.nfields} {← dumpExpr rule.rhs}"
+      return .nil
+    return [(toJson (c, Lean.ConstantInfo.recInfo val))]
--- Main.lean
+++ Main.lean
@@ -3,7 +3,7 @@ open Lean
 
 def semver := "0.1.2"
 
-def main (args : List String) : IO Unit := do
+def main (args : List String) : IO UInt32 := do
   initSearchPath (← findSysroot)
   let (imports, constants) := args.span (· != "--")
   let imports := imports.toArray.map fun mod => { module := Syntax.decodeNameLit ("`" ++ mod) |>.get! }
@@ -11,7 +11,12 @@ def main (args : List String) : IO Unit := do
   let constants := match constants.tail? with
     | some cs => cs.map fun c => Syntax.decodeNameLit ("`" ++ c) |>.get!
     | none    => env.constants.toList.map Prod.fst |>.filter (!·.isInternal)
-  M.run env do
-    IO.println semver
+  let n ← M.run env do
+    let mut n := []
     for c in constants do
-      let _ ← dumpConstant c
+      let a ← dumpConstant c
+      n := List.append n a
+    return n
+  let stdout ← IO.getStdout
+  stdout.writeJson (toJson n)
+  return 0
